9c95e4788d6d5a1c1bfa75cf014c94198f6f88d4
TobyZedomi <tobyzedo4@gmail.com>
2024-04-16 21:06:58 +0100

    app

--- src/main/java/QueueApp/App.java
+++ src/main/java/QueueApp/App.java
@@ -13,49 +13,6 @@ public class App {
 
         Scanner keyboard = new Scanner(System.in);
 
-        /*
-        Queue t1 = new Queue(6);
-
-        t1.add(String.valueOf(new Task("Toby", "Lecture", LocalDate.of(2025,12,11))));
-        t1.add(String.valueOf(new Task("Jack", "Footballer", LocalDate.of(2035,11,17))));
-        t1.add(String.valueOf(new Task("Sam", "Basketball", LocalDate.of(2055,10,24))));
-        t1.add(String.valueOf(new Task("John", "Red", LocalDate.of(2050,8,3))));
-
-
-//// size
-        System.out.println(t1.size());
-        System.out.println("");
-
-
-        //get first element
-        System.out.println(t1.peek());
-        System.out.println("");
-
-
-
-
-/// retrieve queue
-        while(!t1.isEmpty()){
-
-            System.out.println(t1.remove());
-        }
-        System.out.println("");
-
-
-        System.out.println(t1.remove());
-        System.out.println("");
-
-
-        while(!t1.isEmpty()){
-
-            System.out.println(t1.remove());
-        }
-        System.out.println("");
-
-
-
- */
-
 
         String[] menu = new String[6];
 
